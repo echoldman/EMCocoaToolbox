bfd47cdcbe831e1bc9527cde4f816ff73cd87f0f
Echoldman <echoldman@gmail.com>
2013-01-22 23:35:08 +0800

    Create the EMCocoaToolbox project.

---
+++ EMCocoaToolbox/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EMCocoaToolbox
+//
+//  Created by 董 斌 on 13-1-22.
+//  Copyright (c) 2013年 echold.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
