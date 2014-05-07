e5ebf2fab32379f8118a1d5ad9fb65cfe3ca7bcc
Dong Bin <echoldman@gmail.com>
2014-05-07 18:18:29 +0800

    Add new interface 'NSObject:safeObjectInSteps:' and 'NSObject:setObject:inSteps:'.

--- EMCocoaToolbox/NSObjectEx/NSObject+Steps.h
+++ EMCocoaToolbox/NSObjectEx/NSObject+Steps.h
@@ -9,5 +9,11 @@
 @interface NSObject (Steps)
 
 - (id)objectInSteps:(NSArray *)steps;
+- (id)safeObjectInSteps:(NSArray *)steps;
+
+#if (__MAC_OS_X_VERSION_MAX_ALLOWED > __MAC_10_9 || __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_7_1)
+    #warning setObject:inSteps: maybe not safe!
+#endif
+- (id)setObject:(id)object inSteps:(NSArray *)steps;
 
 @end
--- EMCocoaToolbox/NSObjectEx/NSObject+Steps.m
+++ EMCocoaToolbox/NSObjectEx/NSObject+Steps.m
@@ -5,8 +5,14 @@
 //
 
 #import "NSObject+Steps.h"
+#import <objc/runtime.h>
 
 static inline id objectInStep(id container, id step);
+static inline void setObjectInStep(id container, id step, id object);
+static inline void setMutableArrayDepth(NSMutableArray *array, NSInteger depth);
+static inline id insureContainerInStep(id root, id step, id typeStep);
+static inline id makeContainerByTypeStep(id typeStep);
+static inline BOOL isMutableContainer(id container);
 
 static inline id objectInStep(id container, id step)
 {
@@ -26,6 +32,81 @@ static inline id objectInStep(id container, id step)
     return result;
 }
 
+static inline void setObjectInStep(id container, id step, id object)
+{
+    NSString *className = NSStringFromClass([container class]);
+    
+    if ([className isEqualToString:@"__NSDictionaryM"]) {
+        
+        [container setObject:object forKey:step];
+        
+        return;
+    }
+    
+    if ([className isEqualToString:@"__NSArrayM"]) {
+        
+        NSMutableArray *array = (NSMutableArray *)container;
+        NSInteger index = [(NSNumber *)step integerValue];
+        
+        setMutableArrayDepth(array, index + 1);
+        [array setObject:object atIndexedSubscript:index];
+        
+        return;
+    }
+}
+
+static inline void setMutableArrayDepth(NSMutableArray *array, NSInteger depth)
+{
+    NSInteger count = array.count;
+    if (count < depth) {
+        for (NSInteger i = 0; i < array.count - depth ; i++) {
+            [array addObject:[NSNull null]];
+        }
+    }
+}
+
+static inline id insureContainerInStep(id root, id step, id typeStep)
+{
+    id container = objectInStep(root, step);
+    
+    if (!container) {
+        container = makeContainerByTypeStep(typeStep);
+        setObjectInStep(root, step, container);
+        return container;
+    }
+    
+    if (!isMutableContainer(container)) {
+        container = [container mutableCopy];
+        setObjectInStep(root, step, container);
+        return container;
+    }
+    
+    return container;
+}
+
+static inline id makeContainerByTypeStep(id typeStep)
+{
+    id container;
+    
+    if ([typeStep isKindOfClass:[NSString class]]) {
+        container = [NSMutableDictionary dictionary];
+        return container;
+    }
+    
+    if ([typeStep isKindOfClass:[NSNumber class]]) {
+        container = [NSMutableArray array];
+        return container;
+    }
+    
+    return nil;
+}
+
+static inline BOOL isMutableContainer(id container)
+{
+    NSString *className = NSStringFromClass([container class]);
+    return [className isEqualToString:@"__NSDictionaryM"] || [className isEqualToString:@"__NSArrayM"];
+}
+
 @implementation NSObject (Steps)
 
 - (id)objectInSteps:(NSArray *)steps
@@ -37,6 +118,50 @@ static inline id objectInStep(id container, id step)
     }
     
     return result;
+}
+
+- (id)safeObjectInSteps:(NSArray *)steps
+{
+    id result;
+    @try {
+        result = [self objectInSteps:steps];
+    }
+    @catch (NSException *exception) {
+        result = nil;
+    }
+    @finally {}
+    
+    return result;
+}
+
+- (id)setObject:(id)object inSteps:(NSArray *)steps
+{
+    id root = nil;
+
+    if (![self isEqual:[NSNull null]]) {
+        
+        if (isMutableContainer(self)) {
+            root = self;
+        }
+        else {
+            root = [self mutableCopy];
+        }
+    }
+    else {
+        
+        root = makeContainerByTypeStep(steps.firstObject);
+    }
+    
+    id container = root;
+    for (NSInteger i = 0; i < steps.count - 1; i++) {
+        id step = [steps objectAtIndex:i];
+        id nextStep = [steps objectAtIndex:i+1];
+        container = insureContainerInStep(container, step, nextStep);
+    }
+    
+    setObjectInStep(container, steps.lastObject, object);
+    
+    return root;
 }
 
 @end
