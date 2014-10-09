ccc1b4740e3a2f2c0bbdc057f3a1d2caebb455b0
Dong Bin <echoldman@gmail.com>
2014-10-09 11:39:48 +0800

    Update the file NSObject+Steps.h

--- EMCocoaToolbox/NSObjectEx/NSObject+Steps.h
+++ EMCocoaToolbox/NSObjectEx/NSObject+Steps.h
@@ -6,7 +6,8 @@
 
 #import <Foundation/Foundation.h>
 
-#define N(n) [NSNumber numberWithInteger:n]
+#define N(n)    [NSNumber numberWithInteger:n]
+#define S(...)  @[__VA_ARGS__]
 NSArray *EMSteps(NSObject *firstStep, ...);
 
 @interface NSObject (Steps)
@@ -14,7 +15,7 @@ NSArray *EMSteps(NSObject *firstStep, ...);
 - (id)objectInSteps:(NSArray *)steps;
 - (id)safeObjectInSteps:(NSArray *)steps;
 
-#if (__MAC_OS_X_VERSION_MAX_ALLOWED > __MAC_10_9 || __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_7_1)
+#if (__MAC_OS_X_VERSION_MAX_ALLOWED > __MAC_10_10 || __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_8_0)
     #warning setObject:inSteps: maybe not safe!
 #endif
 - (id)setObject:(id)object inSteps:(NSArray *)steps;
